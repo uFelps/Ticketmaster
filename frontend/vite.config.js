9f7cfcf053a0150c6140903a70332ee389f55561
uFelps <felipesand824@gmail.com>
2024-02-28 16:29:57 -0300

    Ticketmaster 1.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ticketmaster.ashyisland-d656732b.brazilsouth.azurecontainerapps.io",
+  base: "https://ticketmasterbr.netlify.app",
 });
